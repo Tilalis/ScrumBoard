cf6d02c60431f0602dca077425725c7fec202e01
Tilalis Eldar <alexvolshtein@gmail.com>
2017-10-19 18:07:22 +0300

    Working server

---
+++ server/logger.js
@@ -0,0 +1,60 @@
+const util = require('util');
+
+const colors = {
+  DEFAULT: "\x1b[0m",
+  BLACK: "\x1b[30m",
+  RED: "\x1b[31m",
+  GREEN: "\x1b[32m",
+  YELLOW: "\x1b[33m",
+  BLUE: "\x1b[34m",
+  MAGNETA: "\x1b[35m",
+  CYAN: "\x1b[36m",
+  WHITE: "\x1b[37m"
+}
+
+const levels = {
+  ERROR: {
+    value: 0,
+    text: "ERROR"
+  },
+  INFO: {
+    value: 1,
+    text: "INFO"
+  },
+  LOG: {
+    value: 2,
+    text: "LOG"
+  }
+}
+
+var currentLevel = levels.LOG;
+
+function setLevel(level) {
+  currentLevel = level;
+}
+
+function _log(color, level, method, at, message, out) {
+  if (level.value <= currentLevel.value) {
+    out(color + level.text + " @ " + method +  ":" + at + ": " + message + colors.DEFAULT);
+  }
+}
+
+function info(method, at, message) {
+  _log(colors.CYAN, levels.INFO, method, at, message, console.info);
+}
+
+function error(method, at, message) {
+  _log(colors.RED, levels.ERROR, method, at, message, console.error);
+}
+
+function log(method, at, message) {
+  _log(colors.YELLOW, levels.LOG, method, at, message, console.log);
+}
+
+module.exports = {
+  info: info,
+  error: error,
+  log: log,
+  setLevel: setLevel,
+  levels: levels
+}
--- server/server.js
+++ server/server.js
@@ -1,23 +1,125 @@
-const express = require('express');
+const express    = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose   = require('./mongoose');
+const logger     = require('./logger');
+
+logger.setLevel(logger.levels.ERROR);
+
 var app = express();
 
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use(express.methodOverride()); // поддержка put и delete
+const status = {
+  ERROR: "ERROR",
+  OK: "OK",
+  NOTFOUND: "NOT FOUND"
+}
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('I AM ALIVE!');
 });
 
 app.get('/api/projects', function(req, res) {
-    res.send('This is not implemented now');
+  return mongoose.ProjectModel.find(function (err, projects) {
+    if (!err) {
+      logger.info(req.method, req.path, "Successfully send projects.");
+      return res.send(projects);
+    } else {
+      res.statusCode = 500;
+      logger.error(req.method, req.path, err.message);
+      return res.send({status: status.ERROR});
+    }
+  })
+});
+
+app.get('/api/projects/:id', function (req, res) {
+  return mongoose.ProjectModel.findById(req.params.id, function (err, project) {
+    if (!err) {
+      if (!project) {
+        res.statusCode = 404;
+        logger.log(req.method, req.path, status.NOTFOUND);
+        return res.send({status: status.NOTFOUND});
+      } else {
+        logger.info(req.method, req.path, "Successfully send project.");
+        return res.send(project);
+      }
+    } else {
+      res.statusCode = 500;
+      logger.error(req.method, req.path, err.message);
+      return res.send({status: status.ERROR});
+    }
+  })
 });
 
 app.post('/api/projects', function(req, res) {
-    res.send('This is not implemented now');
+  var project = new mongoose.ProjectModel(req.body);
+
+  project.save(function (err) {
+    if (!err) {
+      logger.info(req.method, req.path, "Added project.");
+      res.send({status: status.OK});
+    } else {
+      res.statusCode = 500;
+      logger.error(req.method, req.path, err.message);
+      res.send({status: status.ERROR});
+    }
+  })
+});
+
+app.put('/api/projects/:id', function (req, res) {
+  return mongoose.ProjectModel.findById(req.params.id, function (err, project) {
+    if (!err) {
+      if (!project) {
+        res.statusCode = 404;
+        logger.log(req.method, req.path, status.NOTFOUND);
+        return res.send({status: status.NOTFOUND});
+      } else {
+        Object.assign(project, req.body);
+        return project.save(function (err) {
+          if (!err) {
+            logger.info(req.method, req.path, "Updated project");
+            return res.send({status: status.OK});
+          } else {
+            res.statusCode = 500;
+            logger.error(req.method, req.path, err.method);
+            return res.send({status: status.ERROR});
+          }
+        })
+      }
+    } else {
+      res.statusCode = 500;
+      logger.error(req.method, req.path, err.message);
+      return res.send({status: status.ERROR});
+    }
+  });
 });
 
 app.delete('/api/projects/:id', function (req, res){
-    res.send('This is not implemented now');
+  return mongoose.ProjectModel.findById(req.params.id, function (err, project) {
+    if (!err) {
+      if (!project) {
+        res.statusCode = 404;
+        logger.log(req.method, req.path, status.NOTFOUND);
+        return res.send({staus: status.NOTFOUND})
+      } else {
+        return project.remove(function (err) {
+          if (!err) {
+            logger.info(req.method, req.path, "Removed project.");
+            return res.send({staus: status.OK});
+          } else {
+            res.statusCode = 500;
+            logger.console.error(req.method, req.path, err.message);
+            return res.send({status: status.ERROR});
+          }
+        })
+      }
+    } else {
+      res.statusCode = 500;
+      logger.error(req.method, req.path, err.message);
+      return res.send({staus: status.ERROR})
+    }
+  })
 });
 
 app.listen(1337, function(){
